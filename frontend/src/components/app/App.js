faa79088258e1a6bd7e0bc7e0fe923c7b5bb014d
xAmiBa <aminaba666@gmail.com>
2024-04-10 14:35:37 +0100

    test suite for homepage

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 import React from "react";
-import { useNavigate, Routes, Route } from "react-router-dom";
+import { useNavigate, Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from '../HomePage/HomePage';
 import Setup from '../Setup/Setup';
 
 function App() {
+  
   return (
-    <div>
+    <BrowserRouter>
       <Routes>
           <Route path="/" element={ <HomePage navigate={ useNavigate() } />} />
           <Route path="/setup" element={ <Setup navigate={ useNavigate() } />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
 
